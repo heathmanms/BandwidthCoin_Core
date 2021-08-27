4b468b63d868b44b6aa1d88c397933da26584527
Michael Heathman <michael.heathman@yahoo.com>
2021-08-26 22:06:28 -0500

    VS2021 Solution Creation
    Created the initial solution using VS 2021 (VS Community Preview)

---
+++ Launch.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace BandwidthCoin
+{
+    partial class Launch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Launch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 450);
+            this.Name = "Launch";
+            this.Text = "Launcher";
+            this.Load += new System.EventHandler(this.Launch_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Launch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BandwidthCoin
+{
+    public partial class Launch : Form
+    {
+        public Launch()
+        {
+            InitializeComponent();
+        }
+
+        private void Launch_Load(object sender, EventArgs e)
+        {
+            /*
+             * This is a project by a novice. I imagine someone 
+             * will come along eventually and add some amazing 
+             * things to this repository.
+             */
+        }
+    }
+}
